996a7aa4aa8d6974a9628feb1b6f4320733a3afa
ftem2002 <91579449+ftem2002@users.noreply.github.com>
2021-12-24 00:28:22 +0300

    Add files via upload

---
+++ if_statment_max.java
@@ -0,0 +1,48 @@
+package scanner_swing;
+
+public class if_statment_max 
+{
+
+	public static void main(String[] args)
+	{
+		int a , b , c ;
+		a = 8 ;
+		b = 6 ;
+		c = 9 ;
+		
+		 if  (( a >= b) && (a >= c))
+		System.out.println( " the max num is " + a);
+		
+		if (( b >= a ) && (b >= c))
+		System.out.println( " the max num is " + b);
+		
+		if (( c >= a ) && ( c >= b ))
+		System.out.println( " the max num is " + c);
+		int a , b , c ;
+		a = 8 ;
+		b = 6 ;
+		c = 9 ;
+		
+		 if  (( a >= b) && (a >= c))
+		System.out.println( " the max num is " + a);
+		
+		if (( b >= a ) && (b >= c))
+		System.out.println( " the max num is " + b);
+		
+		if (( c >= a ) && ( c >= b ))
+		System.out.println( " the max num is " + c);
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
